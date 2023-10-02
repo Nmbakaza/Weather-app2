9c91bb3762a1c9c9f0c08f05ea48ddd7d5f30876
Nmbakaza <142990946+Nmbakaza@users.noreply.github.com>
2023-10-02 14:50:28 +0200

    Added JS and on page

--- Src/app.js
+++ Src/app.js
@@ -1,9 +1,46 @@
-function displayTemperature(response) {
-  console.log(response.data.main.temp);
-}
+import axios from "axios";
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 let apiKey = "728daf944cfe919571a57ef2b9e8b264";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid={API key}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
+  apiKey;
 
-axious.get(url).then(displayTemperature);
+function showTemp(response) {
+  let tempElement = document.querySelector("#temperature");
+  let tempDesc = document.querySelector("#temp-description");
+  let Humidity = document.querySelector("#Humidity");
+  let Wind = document.querySelector("#Wind");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  tempDesc.innerHTML = response.data.weather[0].description;
+  Humidity.innerHTML = "Humidity: " + response.data.main.humidity;
+  Wind.innerHTML = "Wind: " + Math.round(response.data.wind.speed) + "km";
+}
+
+let date = document.querySelector("#showDate");
+let currentTime = new Date();
+date.innerHTML = getDayTime(currentTime);
+
+function getDayTime(date) {
+  let showtime =
+    days[date.getDay()] + " " + date.getHours() + ":" + date.getMinutes();
+  return showtime;
+}
+let citySearch = document.querySelector("#btnSearch");
+citySearch.addEventListener("click", SearchCity);
+function SearchCity(event) {
+  event.preventDefault();
+  let cityNameElement = document.querySelector("#cityName");
+  cityNameElement.innerHTML = document.getElementById("citySearch").value;
+  let city = cityNameElement.innerHTML;
+  axios.get(apiUrl + "&q=" + city).then(showTemp);
+}
